fd15c0fbc39b2cafac0468a1cb43e5a0347a1776
Patrick Pluequett <ppluequett@gmail.com>
2024-03-28 17:21:12 +0100

    clean up

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,9 +16,6 @@ export async function getUserWithId(req, res, next) {
 export async function createUser(req, res, next) {
     const { first_name, last_name, email } = req.body;
     try {
-        if (!first_name || !last_name || !email) {
-            return res.sendStatus(400);
-        }
         const { rows: user } = await pool.query(
             'INSERT INTO users (first_name, last_name, email) VALUES ($1, $2, $3) RETURNING *;',
             [first_name, last_name, email]
@@ -31,19 +28,8 @@ export async function createUser(req, res, next) {
 
 export async function updateUser(req, res, next) {
     const { first_name, last_name, email } = req.body;
-    if (!first_name || !last_name || !email) {
-        return res.sendStatus(400);
-    }
 
     try {
-        const { rows: user } = await pool.query(
-            'SELECT * FROM users WHERE id = $1',
-            [req.user.id]
-        );
-        if (!user.length > 0) {
-            return res.sendStatus(404);
-        }
-
         const { rows: updatedUser } = pool.query(
             'UPDATE users SET first_name = $1, last_name = $2, email = $3 WHERE id = $4 RETURNING *',
             [first_name, last_name, email, req.user.id]
