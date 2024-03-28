0a626b5399e5444f48377eeaf7ea2c18365140e8
Patrick Pluequett <ppluequett@gmail.com>
2024-03-28 16:26:45 +0100

    add morgan logger

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { userRouter } from './routes/userRouter.js';
 import { errorHandler } from './middlewares/errorHandler.js';
+import morgan from 'morgan';
 
 const app = express();
 
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
